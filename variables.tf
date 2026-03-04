4b63e98a7452568529a34693823c53b7d1c8b8e5
JayaShreeCloudTechnology <jayashreecloudtechnology@gmail.com>
2026-03-04 08:50:12 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -3,9 +3,9 @@ variable "aws_region" {
 }
 
 variable "instance_type" {
-  default = "t2.micro"
+  default = "t3.micro"
 }
 
 variable "ami_id" {
-  default = "ami-0f58b397bc5c1f2e8" # Amazon Linux 2 (ap-south-1)
+  default = "ami-051a31ab2f4d498f5" # Amazon Linux 3 (ap-south-1)
 }
